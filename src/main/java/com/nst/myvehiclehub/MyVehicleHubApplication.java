8c2618457251d9e95701749152587229c4093f6f
nistorbgd <nistorbogdan2019@yahoo.com>
2025-10-27 19:57:33 +0200

    #1 initialize project

---
+++ src/main/java/com/nst/myvehiclehub/MyVehicleHubApplication.java
@@ -0,0 +1,13 @@
+package com.nst.myvehiclehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVehicleHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyVehicleHubApplication.class, args);
+    }
+
+}
