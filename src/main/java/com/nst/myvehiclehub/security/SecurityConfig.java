e56dc984ce4c3f72515a9e31a0c26f0f55c3a9cc
nistorbgd <nistorbogdan2019@yahoo.com>
2025-12-03 01:11:37 +0200

    #1 implement SecurityFilterChain

--- src/main/java/com/nst/myvehiclehub/security/SecurityConfig.java
+++ src/main/java/com/nst/myvehiclehub/security/SecurityConfig.java
@@ -1,23 +1,30 @@
-//package com.nst.myvehiclehub.security;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.authorizeHttpRequests(
-//                requests ->
-//                        requests.anyRequest()
-//                                .authenticated()
-//        ).httpBasic(withDefaults());
-//
-//        return http.build();
-//    }
-//}
+package com.nst.myvehiclehub.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(customizer -> customizer.disable())
+                .authorizeHttpRequests(
+                requests -> requests.anyRequest().authenticated())
+                //.formLogin(Customizer.withDefaults()) //for browser login
+                .httpBasic(Customizer.withDefaults()) //for postman tests
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // create a new session for every request - works with postman, but not with browser
+                .build();
+
+    }
+}
